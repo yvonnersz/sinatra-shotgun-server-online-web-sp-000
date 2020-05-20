aeb0c71b232121065bcd73f34895330ea2719102
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-20 04:37:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS. HELLO?"
+    "Welcome to your app!!!! I BUILT THIS. HELLO? at least i dont have to ctrl c anymore."
   end
 
 end
